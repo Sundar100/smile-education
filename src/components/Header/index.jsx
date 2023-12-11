ff58b14e35621b066e42e85a7607a69980f8276d
JP007-star <justice_of_peace@yahoo.com>
2023-12-11 17:23:16 +0530

    Header Part Inital Commit

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 
+import Call from '../../assets/images/call.png';
+import Mail from '../../assets/images/mail.png';
+
+//Styles
+import '../../assets/css/responsive.css'
+import '../../assets/css/style.css'
+
 /**
 * @author
 * @function Header
 **/
 
-export const Header = (props) => {
+export const Header = () => {
   return(
-    <div>Header</div>
+      <section id="desktop_navbar" class="header_area">
+         <div class="header_top">
+            Header
+            <div class="container">
+               <div class="header_top_wrapper d-flex justify-content-center justify-content-md-between">
+                  <div class="header_top_info d-none d-md-block">
+                     <ul>
+                        <li><img src={Call} alt="call"/><a href="#">+91 9441314306</a></li>
+                        <li><img src={Mail} alt="mail"/><a href="#">sreenathreddy961@gmail.com</a></li>
+                     </ul>
+                  </div>
+                  <div class="header_top_login">
+                     <ul>
+                        <li style = {{color: 'white'}}> Price drop. Hurry up!</li>
+                        <li><a class="main-btn" href="courses.html">Learn More</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
    )
   }
